b901363c3e95f99e60db01489d8d5cab262d6834
David Frühbuß <david.fruehbuss@outlook.de>
2023-04-30 11:42:59 +0200

    same as last commit

--- DD-VAE V2/models.py
+++ DD-VAE V2/models.py
@@ -7,7 +7,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-torch.distributions.categorical
+torch.distributions.categorical.Categorical as Categorical
 
 # Wandb
 import wandb
